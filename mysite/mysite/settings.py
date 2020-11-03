ce8bcea0be648243759d0052d565b55b57b698d8
mnurda <trim.arts007@gmail.com>
2020-11-03 22:55:37 +0600

    Static problem changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
 
-
 ]
 
 MIDDLEWARE = [
@@ -122,6 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/'),
-    os.path.join(BASE_DIR, '/upload/'),
+    os.path.join(BASE_DIR, '/static/css'),
+    os.path.join(BASE_DIR, 'upload'),
 ]
