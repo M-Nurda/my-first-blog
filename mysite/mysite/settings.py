6578366122eef75c02f3a6370c72a9484b3486c4
M-Nurda <trim.arts007@gmail.com>
2020-11-19 01:22:56 +0600

    Kazheetsya nashel reshenie

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,6 +123,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static/'),
 ]
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+# STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'sitestatic')
 ###FUCK PYTHON ANYWHERE
